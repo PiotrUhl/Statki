ebb272d5bb1ac4e7d8395173942dbae81b90b279
Piotr Uhl <piotruhl@gmail.com>
2019-04-05 22:07:11 +0200

    Skonfigurowano pomocniczy projekt testowy CppCLI Włączono możliwość debuggowania dla projektu core

---
+++ CppCLI/main.cpp
@@ -0,0 +1,11 @@
+#include "..\core\DLLinterface.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Running program..." << endl;
+	DLLinterface::runProgram();
+	cout << "Program fishished" << endl;
+	return 0;
+}
