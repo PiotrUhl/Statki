821d139af1d18f425a7aff48886bce82d12ad4a4
Piotr Uhl <piotruhl@gmail.com>
2019-06-06 17:25:51 +0200

    Rozpoczęto tworzenie GUI

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -17,9 +17,59 @@ namespace GUI {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
+
+	struct Ship {
+		public int size;
+		public int x;
+		public int y;
+		public char direction;
+		public bool sunk;
+		public System.Windows.Shapes.Rectangle drawObj;
+	}
+
 	public partial class MainWindow : Window {
+		private Ship[] shipList = new Ship[10];
+		private bool[,] shootedMap = new bool[10,10];
 		public MainWindow() {
+			#region Debug
+			shipList = new Ship[10];
+			shipList[0].x = 6;
+			shipList[0].y = 1;
+			shipList[0].size = 4;
+			shipList[0].direction = 'V';
+			#endregion
 			InitializeComponent();
+		}
+
+		private void TestButton_Click(object sender, RoutedEventArgs e) {
+			RenderNewShip(0);
+			LeftGrid.IsEnabled = true;
+		}
+
+		private void RenderNewShip(int number) {
+			shipList[number].drawObj = new Rectangle {
+				Margin = new Thickness(5),
+				Stroke = Brushes.Black,
+				Fill = Brushes.Black,
+				RadiusX = 30,
+				RadiusY = 30
+			};
+			shipList[number].drawObj.SetValue(Grid.ColumnProperty, shipList[number].x);
+			shipList[number].drawObj.SetValue(Grid.RowProperty, shipList[number].y);
+			if (shipList[number].direction == 'H') {
+				shipList[number].drawObj.SetValue(Grid.ColumnSpanProperty, shipList[number].size);
+				shipList[number].drawObj.SetValue(Grid.RowSpanProperty, 1);
+			}
+			else {
+				shipList[number].drawObj.SetValue(Grid.ColumnSpanProperty, 1);
+				shipList[number].drawObj.SetValue(Grid.RowSpanProperty, shipList[number].size);
+			}
+			shipList[number].drawObj.Visibility = Visibility.Visible;
+			LeftGrid.Children.Add(shipList[number].drawObj);
+		}
+
+		private void Square_Click(object sender, RoutedEventArgs e) {
+			MessageBox.Show(".");
 		}
 	}
 }
