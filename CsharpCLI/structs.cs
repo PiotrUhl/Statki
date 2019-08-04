79ddf4ae8335efa4d1b78da4db2bf8fdd1cb191d
Piotr Uhl <piotruhl@gmail.com>
2019-08-05 00:26:37 +0200

    Przygotowano projekt C#CLI do pracy

---
+++ CsharpCLI/structs.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CsharpCLI {
+
+	public enum PlayerType : byte { NONE = 0, HUMAN, AI, REMOTE };
+
+	public enum ShotResult : byte { NONE = 0, MISS, HIT, SUNK };
+	
+	[StructLayout(LayoutKind.Sequential)]
+	public struct Point {
+		public int x;
+		public int y;
+	};
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct InitData {
+		public PlayerType player1type;
+		public PlayerType player2type;
+	};
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct ShipInfo {
+		public int size;
+		public int x;
+		public int y;
+		public char direction;
+		public bool sunk;
+	};
+}
