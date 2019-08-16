5d536c6840dcb26af5007d617bdffbade9d33a3b
Piotr Uhl <piotruhl@gmail.com>
2019-08-17 00:07:51 +0200

    Rozbudowa klasy Board

--- NewGUI/App.xaml.cs
+++ NewGUI/App.xaml.cs
@@ -11,21 +11,42 @@ namespace NewGUI {
 
 	public partial class App : Application {
 
+		Board board1;
+		Board board2;
+
 		App() {
 			InitializeComponent();
 		}
 
-		[STAThread]
+		[STAThread] //main
 		static void Main(string[] args) {
 			App app = new App();
 			View.MainWindow mainWindow = new View.MainWindow();
 			app.Run(mainWindow);
 		}
 
+		//rozpoczyna grę
 		public static void startGame() {
 			MessageBox.Show("Game started"); //debug
 		}
 
+		//zwraca referencję na planszę o danym id
+		private Board selectBoard(int boardId) {
+			if (boardId == 1)
+				return board1;
+			else if (boardId == 2)
+				return board2;
+			else
+				throw new ArgumentException("Invalid boardId");
+		}
+		//callback do tworzenia front-endowej wersji planszy
+		public void boardCreated(int boardId) {
+			Board board = selectBoard(boardId);
+			board = new Board(boardId, new View.BoardControl());
+			List<ShipInfo> list = DllInterface.getShipList(boardId);
+			foreach (var k in list) {
+				board.add(k);
+			}
+		}
 	}
-
 }
--- NewGUI/Board.cs
+++ NewGUI/Board.cs
@@ -7,9 +7,33 @@ using System.Threading.Tasks;
 namespace NewGUI {
 	class Board {
 
-		View.BoardControl view; //powiązana kontrolka w widoku
-		int boardId; //id planszy
-		List<ShipInfo> shipList; //lista statków na planszy
+		private readonly int boardId; //id planszy
+		private View.BoardControl view; //powiązana kontrolka w widoku
+		private List<ShipInfo> shipList; //lista statków na planszy
+
+		//konstruktor
+		public Board(int _boardId, View.BoardControl _view) {
+			boardId = _boardId;
+			view = _view;
+		}
+
+		//dodaje statek do planszy
+		public void add(ShipInfo ship) {
+			shipList.Add(ship);
+			view.addShip(ship);
+		}
+
+		//usuwa statek z planszy
+		public void remove(ShipInfo ship) {
+			shipList.Remove(ship);
+			view.removeShip(ship);
+		}
+
+		//usuwa wszystkie statki z planszy
+		public void clear() {
+			shipList.Clear();
+			view.clearShips();
+		}
 
 	}
 }
